2c3f81c30d4413abc8bcf963c579adecd37939e6
Hakan Tuncer <hakantuncer@gmail.com>
2020-12-24 22:16:46 +0100

    make reducer and routing a bit smarter

--- apps/onboarding/src/app/services/questionnaire-service.ts
+++ apps/onboarding/src/app/services/questionnaire-service.ts
@@ -1,32 +1,55 @@
 import questionnaire, { QuestionModel } from '../questionnaire/questionnaire'
 
+const questionnaireSize = questionnaire.length;
+
 export function getQuestion(id: string): QuestionModel | undefined {
     return questionnaire.find(q => q.id === id);
+}
+
+function getFirstQuestion(): QuestionModel {
+    return questionnaire[0];
+}
+
+function getNextQuestion(currentQuestionId: string): QuestionModel {
+    const currentIndex = questionnaire.findIndex(q => q.id === currentQuestionId);
+    const nextIndex = currentIndex + 1;
+    return questionnaireSize > nextIndex ?
+        questionnaire[nextIndex] :
+        questionnaire[currentIndex];
+}
+
+function getPreviousQuestion(currentQuestionId: string): QuestionModel {
+    const currentIndex = questionnaire.findIndex(q => q.id === currentQuestionId);
+    const nextIndex = currentIndex - 1;
+    return nextIndex > -1 ?
+        questionnaire[nextIndex] :
+        questionnaire[currentIndex];
 }
 
 export function questionnaireStateReducer(state, action) {
     switch (action.type) {
         case "reset":
             return {
-                currentQuestionId: 1,
+                currentQuestionId: getFirstQuestion().id,
                 answers: []
             };
         case "set-question-id": {
             const { questionId } = action;
+            const question = getQuestion(questionId);
             return {
                 ...state,
-                currentQuestionId: parseInt(questionId)
+                currentQuestionId: question?.id ?? getFirstQuestion().id 
             };
         }
         case "next-question":
             return {
                 ...state,
-                currentQuestionId: state.currentQuestionId + 1
+                currentQuestionId: getNextQuestion(state.currentQuestionId).id
             }
         case "previous-question":
             return {
                 ...state,
-                currentQuestionId: state.currentQuestionId - 1 ?? 1
+                currentQuestionId: getPreviousQuestion(state.currentQuestionId).id
             }
         default:
             throw new Error("Unhandled action " + action.type);
