7cc18aac2d812ef10d4d06fdbc0e1bf6b299bdb3
Hakan Tuncer <hakantuncer@gmail.com>
2021-10-20 21:18:33 +0200

    refactor id check app for angular strict mode

--- apps/idcheck/src/app/id-check/id-check.component.ts
+++ apps/idcheck/src/app/id-check/id-check.component.ts
@@ -3,6 +3,10 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { IdCheckStatus, OnboardingState } from '@customer-onboarding/data';
 
+interface OnboardingResponse {
+  onboardingWorkflow: OnboardingState
+}
+
 @Component({
   selector: 'customer-onboarding-id-check',
   templateUrl: './id-check.component.html',
@@ -23,14 +27,14 @@ export class IdCheckComponent implements OnInit {
     private activatedRoute: ActivatedRoute,
     private httpClient: HttpClient
   ) {
-    this.onboardingId = this.activatedRoute.snapshot.paramMap.get('onboardingId');
-    this.idCheckId = this.activatedRoute.snapshot.paramMap.get('idCheckId');
-    this.idCheckIndex = this.activatedRoute.snapshot.paramMap.get('idCheckIndex');
+    this.onboardingId = this.activatedRoute.snapshot.paramMap.get('onboardingId') as string;
+    this.idCheckId = this.activatedRoute.snapshot.paramMap.get('idCheckId') as string;
+    this.idCheckIndex = this.activatedRoute.snapshot.paramMap.get('idCheckIndex') as string;
   }
 
   ngOnInit(): void {
-    this.httpClient.get(`https://localhost:5001/onboarding/${this.onboardingId}`)
-      .subscribe((response: { onboardingWorkflow: OnboardingState }) => {
+    this.httpClient.get<OnboardingResponse>(`https://localhost:5001/onboarding/${this.onboardingId}`)
+      .subscribe(response => {
         this.currentEvent = response.onboardingWorkflow.idCheckWorkflows.find(
           i => i.idCheckWorkflowId === this.idCheckId
         )?.status ?? IdCheckStatus.Initial;
